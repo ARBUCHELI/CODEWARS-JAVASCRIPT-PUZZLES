2ccae74de3213d45c3bb00182198c7d1db9ec795
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2020-11-05 12:50:14 +0300

    Update kata1.js

--- kata1.js
+++ kata1.js
@@ -1,4 +1,17 @@
-/*Fix the code*/
+/*This code does not execute properly. Try to figure out why.*/
+
 function multiply(a, b){
  return a * b;
 }
+
+/*Test*/
+const assert = require("chai").assert;
+
+describe("Multiply", () => {
+  it("fixed tests", () => {
+    assert.strictEqual(multiply(1,1), 1);
+    assert.strictEqual(multiply(2,1), 2);
+    assert.strictEqual(multiply(2,2), 4);
+    assert.strictEqual(multiply(3,5), 15);   
+  });
+});
