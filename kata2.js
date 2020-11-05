19f4fc9536684dc04b7a1e07888353dcf0dd4a17
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2020-11-05 12:52:11 +0300

    Update kata2.js

--- kata2.js
+++ kata2.js
@@ -13,3 +13,13 @@ function solution(number){
   }
   return sum;
 }
+
+/*Test*/
+function test(n, expected) {
+  let actual = solution(n)
+  Test.assertEquals(actual, expected, `Expected ${expected}, got ${actual}`)
+}
+
+Test.describe("basic tests", function(){
+  test(10,23)
+})
