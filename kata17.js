a047a5f235dc41c66927c2213b68f4c4a59231dd
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2020-11-18 10:20:46 +0300

    Update kata17.js

--- kata17.js
+++ kata17.js
@@ -1,16 +1,18 @@
-/*The number is a basic data type in javascript. javascript also supports Number objects. The object is the original value of the package object. When necessary, JavaScript automatically converts between the original data and the objects. You can explicitly create a Number object with the constructor Number(). Although it is not necessary to do so.
-Usage:*/
+/*The number is a basic data type in javascript. javascript also supports Number objects. The object is the original value of the package object. When necessary, JavaScript
+automatically converts between the original data and the objects. You can explicitly create a Number object with the constructor Number(). Although it is not necessary to do
+so. Usage:*/
 
 var num=new Number(value);
 
 /*Parameter value is the value of the Number object to be created or the value to be converted into a numeric value.
 
-Constructor Number() can be used without operator new and directly as a transformation function to use. In this way, when the Number is called, it transforms itself into a number
-and then returns the converted value (or NaN). Usage:*/
+Constructor Number() can be used without operator new and directly as a transformation function to use. In this way, when the Number is called, it transforms itself into a 
+number and then returns the converted value (or NaN). Usage:*/
 
 var num=Number(value);
 
-/*Number object has two generic object properties: constructor and prototype. all the objects in JS have these two properties. they are two very important attributes. because of their importance and complexity, we will learn it in the future.
+/*Number object has two generic object properties: constructor and prototype. all the objects in JS have these two properties. they are two very important attributes. because 
+of their importance and complexity, we will learn it in the future.
 
 In addition to the above two, the Number objects have five attributes (or called constant):
 
@@ -18,13 +20,17 @@ MAX_VALUE: The maximum number that can be expressed in JS. Usage:Number.MAX_VALU
 
 MIN_VALUE: The minimum number that can be expressed in JS(Close to 0, but not negative). Usage:Number.MIN_VALUE. Its approximate value is 5e-324
 
-NaN: Non numeric value. Abbreviations of "Not a Number". When some arithmetic operations (such as the square root of a negative number) or the result of the method are not numbers, return NaN. Usage:Number.NaN. It can be simplified and replaced with NaN.
+NaN: Non numeric value. Abbreviations of "Not a Number". When some arithmetic operations (such as the square root of a negative number) or the result of the method are not 
+numbers, return NaN. Usage:Number.NaN. It can be simplified and replaced with NaN.
 
-Please note: the results of comparison between NaN and other values are always not equal(including its own). Therefore, can not be compared with Number.NaN to detect a value is not a number but can only call isNaN() to compare.
+Please note: the results of comparison between NaN and other values are always not equal(including its own). Therefore, can not be compared with Number.NaN to detect a value is
+not a number but can only call isNaN() to compare.
 
-NEGATIVE_INFINITY: The value represents the negative infinity. Usage:Number.NEGATIVE_INFINITY. When a number is generated in an arithmetic operation or function and it smaller than -Number.MAX_VALUE return this value. It can be simplified and replaced with -Infinity.
+NEGATIVE_INFINITY: The value represents the negative infinity. Usage:Number.NEGATIVE_INFINITY. When a number is generated in an arithmetic operation or function and it smaller 
+than -Number.MAX_VALUE return this value. It can be simplified and replaced with -Infinity.
 
-POSITIVE_INFINITY: The value represents the positive infinity. Usage:Number.POSITIVE_INFINITY. When a number is generated in an arithmetic operation or function and it larger than Number.MAX_VALUE return this value. It can be simplified and replaced with Infinity.
+POSITIVE_INFINITY: The value represents the positive infinity. Usage:Number.POSITIVE_INFINITY. When a number is generated in an arithmetic operation or function and it larger 
+than Number.MAX_VALUE return this value. It can be simplified and replaced with Infinity.
 
 Ok, lesson is over. let's us do some task with Number objects.
 
