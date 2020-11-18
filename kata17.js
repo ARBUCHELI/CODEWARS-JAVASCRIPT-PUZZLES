3e0066823494dcbdc2e42ed57eae73e35aeae292
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2020-11-18 10:22:02 +0300

    Update kata17.js

--- kata17.js
+++ kata17.js
@@ -23,11 +23,11 @@ MIN_VALUE: The minimum number that can be expressed in JS(Close to 0, but not ne
 NaN: Non numeric value. Abbreviations of "Not a Number". When some arithmetic operations (such as the square root of a negative number) or the result of the method are not 
 numbers, return NaN. Usage:Number.NaN. It can be simplified and replaced with NaN.
 
-Please note: the results of comparison between NaN and other values are always not equal(including its own). Therefore, can not be compared with Number.NaN to detect a value is
-not a number but can only call isNaN() to compare.
+Please note: the results of comparison between NaN and other values are always not equal(including its own). Therefore, can not be compared with Number.NaN to detect a value
+is not a number but can only call isNaN() to compare.
 
-NEGATIVE_INFINITY: The value represents the negative infinity. Usage:Number.NEGATIVE_INFINITY. When a number is generated in an arithmetic operation or function and it smaller 
-than -Number.MAX_VALUE return this value. It can be simplified and replaced with -Infinity.
+NEGATIVE_INFINITY: The value represents the negative infinity. Usage:Number.NEGATIVE_INFINITY. When a number is generated in an arithmetic operation or function and it 
+smaller than -Number.MAX_VALUE return this value. It can be simplified and replaced with -Infinity.
 
 POSITIVE_INFINITY: The value represents the positive infinity. Usage:Number.POSITIVE_INFINITY. When a number is generated in an arithmetic operation or function and it larger 
 than Number.MAX_VALUE return this value. It can be simplified and replaced with Infinity.
