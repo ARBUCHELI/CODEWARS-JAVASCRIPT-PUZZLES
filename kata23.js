4aa544ac0e381908332e86c338d3bea1b30059df
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2020-11-27 18:55:55 +0300

    Update kata23.js

--- kata23.js
+++ kata23.js
@@ -98,7 +98,7 @@ alienLanguage("this is an example") should return "THIs Is An EXAMPLe"
 alienLanguage("Hello World") should return "HELLo WORLd"
 A small hint: The first conversion of the entire string will make the code easier*/
 
-//Solution:
+//Solution: (The Problem is still not solved)
 
 
 
