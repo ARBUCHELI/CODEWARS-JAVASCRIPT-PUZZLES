dee76224e4eb3c18a1dfe8ac21fdb0fb3e60a6d9
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2020-12-21 08:57:49 +0300

    Update kata27.js

--- kata27.js
+++ kata27.js
@@ -69,7 +69,7 @@ infiniteLoop( [[1,2,3],[4,5,6],[7,8,9]],"left",1) should return [[2,3,4],[5,6,7]
 infiniteLoop( [[1,2,3],[4,5,6],[7,8,9]],"right",1) should return [[9,1,2],[3,4,5],[6,7,8]]
 infiniteLoop( [[1,2],[3,4,5,6],[7,8,9,10]],"left",2) should return [[3,4],[5,6,7,8],[9,10,1,2]]
 
-//Solution completed !!
+//Temporary solution !!!
 
 function infiniteLoop(arr,d,n){
   //coding here...
