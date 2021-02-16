e6395d06c52ec4beacfb2f120dabe1a538c0a9b3
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-02-16 10:25:48 +0300

    Update kata5.js

--- kata5.js
+++ kata5.js
@@ -3,7 +3,9 @@
 Numbers can use operators such as + - * / %
 
 Task
-I've written five function equal1,equal2,equal3,equal4,equal5, defines six global variables v1 v2 v3 v4 v5 v6, every function has two local variables a,b, please set the appropriate value for the two variables(select from v1--v6), making these function return value equal to 100. the function equal1 is completed, please refer to this example to complete the following functions.
+I've written five function equal1,equal2,equal3,equal4,equal5, defines six global variables v1 v2 v3 v4 v5 v6, every function has two local variables a,b, please set the
+appropriate value for the two variables(select from v1--v6), making these function return value equal to 100. the function equal1 is completed, please refer to this example to
+complete the following functions.
 
 When you have finished the work, click "Run Tests" to see if your code is working properly.
 
