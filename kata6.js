43e5a950670d7e49d53b2f741f8d412d478eb87f
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-02-16 10:28:45 +0300

    Update and rename kata7.js to kata6.js

--- kata6.js
+++ kata6.js
@@ -1,10 +1,12 @@
 /*
-In javascript, String is one of basic data types. Use "" or '' to declare a string variable. It can be letters:a,b,c,A,B,C, numbers: 123..., Various symbol:@#$%^&* , Control symbol:\n,\t etc..
+In javascript, String is one of basic data types. Use "" or '' to declare a string variable. It can be letters:a,b,c,A,B,C, numbers: 123..., Various symbol:@#$%^&* , Control
+symbol:\n,\t etc..
 
 String can use operators +, Connect two or more strings together.
 
 Task
-misson 1: I've create three function, and defined some global variables, please select some variables that can make up the name of the function, and return them(Please note the uppercase and lowercase letters are different).
+misson 1: I've create three function, and defined some global variables, please select some variables that can make up the name of the function, and return them(Please note the
+uppercase and lowercase letters are different).
 
 When you have finished the work, click "Run Tests" to see if your code is working properly.
 
